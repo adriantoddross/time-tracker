c156f82d233ab4fab03c5c0260a3382673d6f7d9
Adrian Ross <adriantoddross@gmail.com>
2021-08-09 12:41:58 -0400

    Stub activity form

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,13 +1,39 @@
-import { FunctionalComponent, h } from 'preact';
-import style from './style.css';
+import { FunctionalComponent, h } from "preact";
+import style from "./style.css";
 
 const Home: FunctionalComponent = () => {
-    return (
-        <div class={style.home}>
-            <h1>Home</h1>
-            <p>This is the Home component.</p>
-        </div>
-    );
+  return (
+    <div class={style.home}>
+      <h1>Home</h1>
+      <p>This is the Home component.</p>
+
+      <form>
+        <fieldset>
+          <legend>Time</legend>
+          <p>
+            <label for="hour">Hour</label>
+            <input type="number" name="hour" id="hour" min="1" />
+
+            <label for="minute">Minute</label>
+            <input type="number" name="minute" id="minute" min="1" max="59" />
+
+            <label for="seconds">Seconds</label>
+            <input type="number" name="seconds" id="seconds" min="1" max="59" />
+          </p>
+        </fieldset>
+
+        <label for="activity">Activity</label>
+        <input
+          type="text"
+          id="activity"
+          name="activity"
+          placeholder="E.g. Research, Meeting, ..."
+          spellCheck={true}
+          minLength={1}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
